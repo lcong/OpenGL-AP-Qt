389cea2394812e393439ae9040a0ad7c97838464
lcong <rockcong@gmail.com>
2020-12-05 15:35:20 +0800

    从shader资源表里去掉非必要的shader源码。

--- 9.1.geometry_shader_houses/CoreFunctionWidget.cpp
+++ 9.1.geometry_shader_houses/CoreFunctionWidget.cpp
@@ -44,13 +44,13 @@ void CoreFunctionWidget::initializeGL()
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (oid *)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
 
-    glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindVertexArray(0);
 }
