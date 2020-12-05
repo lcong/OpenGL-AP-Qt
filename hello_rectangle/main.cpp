23c3ef2a34f539c28a4d65f089db13e02fd4e3c6
lcong <rockcong@gmail.com>
2020-12-05 16:42:37 +0800

    基于Qt+OpenGL的第一个矩阵绘制实例

---
+++ hello_rectangle/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "GlWidget.h"
+#include <QApplication>
+#include <assimp/Importer.hpp>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    GlWidget w1;
+
+    w1.show();
+    return (a.exec());
+}
