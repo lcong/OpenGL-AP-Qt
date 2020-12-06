950bce760935fd87a74313ca1cfff8582da423fe
lcong <rockcong@gmail.com>
2020-12-06 11:29:41 +0800

    修复遗漏笔误，应该是(void*)

--- 9.1.geometry_shader_houses/CoreFunctionWidget.cpp
+++ 9.1.geometry_shader_houses/CoreFunctionWidget.cpp
@@ -46,7 +46,7 @@ void CoreFunctionWidget::initializeGL()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (oid *)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(2 * sizeof(float)));
